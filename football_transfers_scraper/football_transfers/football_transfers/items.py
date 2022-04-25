5213eff353d7a68799b64d8970c7b7f2c5e43c5d
Adnan Alam <adnan320324@gmail.com>
2022-04-26 01:06:32 +0600

    FootballTransfersItem updated with fields

--- football_transfers_scraper/football_transfers/football_transfers/items.py
+++ football_transfers_scraper/football_transfers/football_transfers/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class FootballTransfersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    data_dict = scrapy.Field()
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
