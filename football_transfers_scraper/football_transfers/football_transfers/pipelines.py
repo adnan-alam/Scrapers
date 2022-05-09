91c966161e1481ee9d63128bf4dcf9685ab8e0de
Adnan Alam <adnan320324@gmail.com>
2022-05-09 22:55:57 +0600

    added FutureStarDataPipeline to save scraped future star data in db

--- football_transfers_scraper/football_transfers/football_transfers/pipelines.py
+++ football_transfers_scraper/football_transfers/football_transfers/pipelines.py
@@ -1,13 +1,35 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
+from sqlalchemy.orm import sessionmaker
+from football_transfers.models import FutureStar, db_engine
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+logger = logging.getLogger()
 
 
-class FootballTransfersPipeline:
+class FutureStarDataPipeline:
+    def __init__(self):
+        # Initializes database connection and session
+        try:
+            self.engine = db_engine
+            self.Session = sessionmaker(bind=self.engine)
+        except Exception as e:
+            logger.exception(e)
+
+    def save_player_data(self, item):
+        try:
+            scraped_data_dict = item["data_dict"]
+            data_to_save = FutureStar(**scraped_data_dict)
+
+            session = self.Session()
+            session.add(data_to_save)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            msg = "ROLLBACK! {}".format(e)
+            logger.exception(msg)
+        finally:
+            session.close()
+
     def process_item(self, item, spider):
+        self.save_player_data(item)
         return item
