bf6cc655fd41f17772231d5c55623e28f5f07332
Adnan Alam <adnan320324@gmail.com>
2022-05-09 22:56:36 +0600

    added database settings, read database settings from .env

--- football_transfers_scraper/football_transfers/football_transfers/settings.py
+++ football_transfers_scraper/football_transfers/football_transfers/settings.py
@@ -11,11 +11,11 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import os
-
+from environs import Env
 from scrapy.utils.log import configure_logging
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 BOT_NAME = "football_transfers"
@@ -75,7 +75,7 @@ DOWNLOAD_DELAY = 0.2
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     "scrapy.pipelines.images.ImagesPipeline": 1,
-    "football_transfers.pipelines.FootballTransfersPipeline": 300,
+    "football_transfers.pipelines.FutureStarDataPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -104,10 +104,16 @@ IMAGES_STORE = os.path.join(BASE_DIR, "images")
 
 
 # log settings
+logs_dir_path = os.path.join(BASE_DIR, "logs")
+if not os.path.isdir(logs_dir_path):
+    os.mkdir(logs_dir_path)
+
 LOG_ENABLED = False
 configure_logging(install_root_handler=False)
-log_file = os.path.join(BASE_DIR, "logs.log")
+
+log_file = os.path.join(logs_dir_path, "logs.log")
 log_filesize = 1024 * 1024 * 2  # 2 MB
+
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
@@ -117,3 +123,23 @@ rotating_file_log = RotatingFileHandler(log_file, maxBytes=log_filesize, backupC
 rotating_file_log.setLevel(logging.INFO)
 rotating_file_log.setFormatter(formatter)
 root_logger.addHandler(rotating_file_log)
+
+
+env = Env()
+env.read_env()  # read .env file, if it exists
+
+
+DB_USERNAME = env.str("DB_USERNAME")
+DB_PASSWORD = env.str("DB_PASSWORD")
+DB_NAME = env.str("DB_NAME")
+
+
+# database settings
+DATABASE = {
+    "drivername": "mysql+pymysql",
+    "host": "localhost",
+    "port": "3306",
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+}
